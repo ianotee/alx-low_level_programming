86010013aa49963ca711b3be965dddd259e23c76
ianotee <oteeian500@gmail.com>
2023-06-15 04:59:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - The program will print the text and adds to a new line.
+ *
+ * Return: 0 if (Successful).
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
