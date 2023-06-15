8105ddb1b8bc41c46584c352197205dc6315cfc1
ianotee <oteeian500@gmail.com>
2023-06-15 01:37:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * The main function will contain the code to be executed.
+ * puts this will printf out our text.
+ * return the program will return a value of zero.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
