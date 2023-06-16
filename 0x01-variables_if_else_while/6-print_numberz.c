f43b31be547fe780666740c5e446e45e088b39b7
ianotee <oteeian500@gmail.com>
2023-06-16 07:03:43 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The main  block.
+ * Description - It prints all numbers of base 10 starting from zero.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
