bc85443dca7bad1dbd99c588aface8e35a358aba
ianotee <oteeian500@gmail.com>
2023-06-20 08:23:12 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @x: int type number
+ * @m: int type number
+ * Return: result of addition
+ */
+
+int add(int x, int m)
+{
+	return (x + m);
+}
