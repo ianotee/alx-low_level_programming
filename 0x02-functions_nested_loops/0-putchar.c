94923bcccb3a966a69210b8519e8800eb4fa1984
ianotee <oteeian500@gmail.com>
2023-06-20 03:42:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
 /**
- * main - This is the main block.
+ * main - This is the main Function.
  *
- * Return: 0 if (Success).
+ * Return: 0 if(successful).
  */
 
 int main(void)
 {
-	_putchar("_putchar\n");
+	char my_Function[] = "_putchar";
+	int i;
 
+	while (my_Function[i] != '\0')
+	{
+		_putchar(my_Function[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
