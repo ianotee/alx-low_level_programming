a73b280cce5de463decb9d2219209a92c84caadd
ianotee <oteeian500@gmail.com>
2023-06-20 14:04:09 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - The main prints a positive or a negative integer.
+ *
+ * Return: 0 if (Successful).
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
