391193dd7ee07b22e6b57755178442fdafe3e651
ianotee <oteeian500@gmail.com>
2023-06-22 12:36:33 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int  main(void)
+{
+	print_number(98);
+	print_number('\n');
+	print_number(402);
+	print_number('\n');
+	print_number(1024);
+	print_number('\n');
+	print_number(0);
+	print_number('\n');
+	print_number(-98);
+	print_number('\n');
+	return (0);
+}
