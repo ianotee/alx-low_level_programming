549619eaa62c0d0dd6f802ebc7ffc105f65c368a
ianotee <oteeian500@gmail.com>
2023-06-26 04:09:46 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - print one char out of 2 of a string.
+ *@str: char param.
+ *Description: Print 1st char, then 3rd, then 5th, etc..
+ * Return: 0 if (successful).
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+
+	_putchar ('\n');
+}
