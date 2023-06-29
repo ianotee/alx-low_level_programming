7bfcd4bc757e4d8f235b9f0899ee4ed191ea6cdb
ianotee <oteeian500@gmail.com>
2023-06-29 11:49:06 -0700

    104-main.c

--- 0x06-pointers_arrays_strings/104-main.c
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -6,16 +6,12 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)",
-	char buffer[] = "\1\2\3\4\5\6\7#cisfun\n\0\0",
-	char buffer[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34",
-	char buffer[] = "\x56#pointersarefun #infernumisfun\n";
+    char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
 
-	printf("%s\n", buffer);
-	printf("---------------------------------\n");
-	print_buffer(buffer, sizeof(buffer));
-	return (0);
+    printf("%s\n", buffer);
+    printf("---------------------------------\n");
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
 }
