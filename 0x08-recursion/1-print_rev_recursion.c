da43d1d2713a80a4f2d7e4d0d2600d5deb22354e
ianotee <oteeian500@gmail.com>
2023-07-05 02:29:17 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,11 +10,10 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	{
+		return;
+
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(--s);
 	}
+
 }
