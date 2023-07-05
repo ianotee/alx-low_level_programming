859124070d118206670e5de51c320e3358f21aec
ianotee <oteeian500@gmail.com>
2023-07-05 02:24:18 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_print_rev_recursion - a function that prints a string in reverse.
+*@s: char param.
+*Return: 0 if (successful)
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_print_rev_recursion(--s);
+	}
+}
