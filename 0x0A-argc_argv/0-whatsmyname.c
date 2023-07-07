215b111aa9582d55d0d89dc6cedf104fec5fc2ee
ianotee <oteeian500@gmail.com>
2023-07-07 07:02:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - The main block of the program.
+ *@argc:  number of arguments to a program.
+ *@argv: array of pointers to the argument
+ *Return: Always o.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
