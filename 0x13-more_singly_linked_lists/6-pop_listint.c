8ec3d5f3d2f2606dc871d907aa08aebcb0422266
ianotee <oteeian500@gmail.com>
2023-08-01 16:34:56 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 {
 	listint_t *shon;
 
-	int x;
+	int n;
 
 
 	shon = *head;
@@ -23,7 +23,7 @@ int pop_listint(listint_t **head)
 
 	*head = shon->next;
 
-	x = shon->x;
+	n = shon->n;
 	free(shon);
-	return (x);
+	return (n);
 }
