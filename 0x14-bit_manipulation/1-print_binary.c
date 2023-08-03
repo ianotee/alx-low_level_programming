19f9324638f4fe1f014e31b9c7d7725327d0fc26
ianotee <oteeian500@gmail.com>
2023-08-03 15:12:42 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,48 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: number in decimal
+ * _pow - calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= power; i++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
  *
  * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int check = 1, w = n, i = 0;
+	unsigned long int divisor, check;
+	char flag;
 
-	if (n == 0)
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
 	{
-		_putchar('0');
-		return;
-	}
-
-	while (w > 0)
-	{
-		i++;
-		w = w >> 1;
-	}
-
-	check = check << (i - 1);
-
-	for (; i > 0; i--)
-	{
-		if (n & check)
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
 			_putchar('1');
-		else
+		}
+		else if (flag == 1 || divisor == 1)
+		{
 			_putchar('0');
-
-		check = check >> 1;
+		}
+		divisor >>= 1;
 	}
 }
