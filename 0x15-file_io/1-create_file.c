be6fe1b614c65a8337ef624ac523bcbf32f76da2
ianotee <oteeian500@gmail.com>
2023-08-07 05:34:26 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	code = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	code = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (code == -1)
 		return (-1);
