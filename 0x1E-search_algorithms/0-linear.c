5e862c409984958a3f32795c98f922ba8ce02f97
ianotee <oteeian500@gmail.com>
2024-01-17 11:12:06 -0800

    second commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,11 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
- *
- * @array: input array
+ * linear_search - searches a value
+ * @array: array
  * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * @value:  search 
+ * Return: EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
